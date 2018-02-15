07ca0fbf03c5e6de2cb2e101d0065fbdc569c8d6
brandonv98 <enjoi98@gmail.com>
2018-02-15 15:21:48 -0600

    n/a

--- github_web_pages/src/App.js
+++ github_web_pages/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Show</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello I am <code>Brandon VanCamp</code>. I like to ...
+          <br/>
+            <br/>
+        <p>Please <a href='#'>Contact Me</a> if you would like to collaborate togather.</p>
         </p>
+
+        <a href='#'><span> GitHub </span></a>
+        <a href='#'><span> Facebook </span></a>
+        <a href='#'><span> Linkedin </span></a>
       </div>
     );
   }
