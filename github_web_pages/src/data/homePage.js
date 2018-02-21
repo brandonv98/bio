0fce7777ca9d8535551cd14e090199bf22a64fb3
brandonv98 <enjoi98@gmail.com>
2018-02-21 11:41:28 -0600

    add data
    add data to new folder "data", for home page in file 'homePage.js'

---
+++ github_web_pages/src/data/homePage.js
@@ -0,0 +1,44 @@
+export const HomePage = [
+  {
+    title:"WORKING WITH US",
+    description:"We deliver web page types developed from HTML markup, CSS stylesheets for styling and unobtrusive JavaScript for feature detection, poly-fills and behaviors.",
+    img_src:"https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage1.png",
+    id: "js-1"
+  },
+  {
+    title:"BUILD SMART",
+    description:"Testing no longer means attempting to make a website look the same in browsers of different capabilities or on devices with different size screens. It does mean ensuring that a person’s experience of a design should be appropriate to the capabilities of a browser or device.",
+    img_src:"https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage5.png",
+    id: "js-2"
+  },
+  {
+    title: "PERFORMANCE",
+    description: "The organization and architecture of a code base can greatly affect not only the speed of development, but also the speed at which pages render. Both of which can be sizeable concerns not only for developers but also users. Taking the time to design the right structure for a code base, and identify how all of the different components will work together, can speed up production and make for a better experience all around.",
+    img_src: "https://achievement-images.teamtreehouse.com/badges_JavaScript_jQueryBasics_Stage1.png",
+    id: "js-3"
+  },
+  {
+    title: "DESIGN",
+    description: "We create look-and-feel designs, and flexible layouts that adapt to the capabilities of many devices and screen sizes. We create designs iteratively and use predominantly HTML and CSS so we won’t waste time mocking up every template as a static visual. We may use visuals to indicate a creative direction (colour, texture and typography.) We call that ‘atmosphere.’",
+    img_src: "https://achievement-images.teamtreehouse.com/badges_JavaScript_AJAXBasics_Stage3.png",
+    id: "js-4"
+  },
+  // {
+  //   title: "Interactive Web Pages with JavaScript",
+  //   description: "In this course we’ll create a to-do list application using JavaScript alone, without using any third party libraries. You’ll get to grips with manipulating and traversing the DOM and adding event handlers to web page elements. The things that you can do with JavaScript are always increasing and this course will give you a solid footing going forward.",
+  //   img_src: "https://achievement-images.teamtreehouse.com/bagdes-javascript-interactiveweb-stage-12.png",
+  //   id: "js-5"
+  // },
+  // {
+  //   title: "Node.js Basics",
+  //   description: "In this course we will create a command line application to retrieve user's profile information from the Treehouse website. We'll be writing our application in JavaScript to run on the Node.js platform.",
+  //   img_src: "https://achievement-images.teamtreehouse.com/badges_JavaScript_nodeBasics_Stage1.png",
+  //   id: "js-6"
+  // }
+];
+
+
+
+
+
+
