8e2860a7a8af294dd114aa1fb0c7e7f938d29e4b
brandonv98 <enjoi98@gmail.com>
2018-02-23 08:35:55 -0600

    BS add

--- github_web_pages/src/components/index.js
+++ github_web_pages/src/components/index.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
-import '../App.css';
+//Components
 import {
   HomePage
 } from '../data/homePage';
+
+// Bootstrap
+import { Grid, Jumbotron, FormGroup, FormControl, Button, Form, Col, Row, Image } from 'react-bootstrap';
+
+
 
 export default class Home extends Component {
 
 
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
+        <Jumbotron>
+          <img src={logo} alt="Photo of Brandon Doe" className="App-logo" />
+          <h1 class="tag name">Hello, I’m Brandon.</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button bsStyle="primary">Learn more</Button>
+          </p>
+        </Jumbotron>;
+        {/* <header className="App-header">
           <img src={logo} alt="Photo of Brandon Doe" class="profile-image" />
           <h1 class="tag name">Hello, I’m Brandon.</h1>
           {/* <p class="tag location">My home is Kansas City, Missouri.</p> */}
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           {/* <h1 className="App-title">Welcome to the Show</h1> */}
-        </header>
+        {/* </header> */}
 
 
         <p className="App-intro">
@@ -26,6 +43,10 @@ export default class Home extends Component {
             <br/>
         <p>Please <a href='#'>Contact Me</a> if you would like to collaborate togather.</p>
         </p>
+
+        {/* <div> */}
+
+        {/* </div> */}
 
 
         {/* line seperator */}
@@ -39,9 +60,9 @@ export default class Home extends Component {
 
         {/* footer */}
 
-        <Route path={`/`}
+        {/* <Route path={`/`}
           render={ () => <CourseContainer data={HTMLCourses} /> }
-        />
+        /> */}
 
 
       </div>
--- github_web_pages/src/index.js
+++ github_web_pages/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'; // Bunch of BS CSS...
+import './index.css'; // Not BS CSS, the good stuff...
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
