ad0b284155cf31534d2f457796698f8a22a98a00
brandonv98 <enjoi98@gmail.com>
2018-03-02 17:07:24 -0600

    Updates

--- github_web_pages/src/components/Projects/Skills.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Skill = (props) => {
-  const lowerCase = (word) => {
-    return word.toLowerCase();
-  }
-  return (
-    <li className={lowerCase(props.skill)}>{props.skill}</li>);
-}
-export default Skill;
