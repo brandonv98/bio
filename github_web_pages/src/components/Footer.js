d53463527a2e75f2459ee748d651498d56ce81c3
Brandon VanCamp <brandon@overtimewebdev.com>
2018-04-03 07:53:02 -0500

    Project title change

--- github_web_pages/src/components/Footer.js
+++ github_web_pages/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 const Footer = (props) => ( // Footer
-  <footer>
+	<footer>
     <span className="divider" />
     <p>Let's work togather!</p>
-    <a className="btn btn-primary" href="mailto:brandon@overtimewebdev.com">Contact Brandon</a>
+    <a className="btn btn-primary" href="mailto:brandon@csdevs.io">Contact Brandon</a>
     <ul>
       <li><a href="https://github.com/brandonv98" rel="noreferrer noopener" target="_blank" className="social github">Github</a></li>
       <li><a href="https://www.linkedin.com/in/brandon-vancamp-123360150/" rel="noreferrer noopener" target="_blank" className="social linkedin">Linkedin</a></li>
