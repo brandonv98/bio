2e7e9f520e9eb9adfdb6ff7a36f313997644af09
Brandon VanCamp <brandon@overtimewebdev.com>
2018-09-27 17:20:32 -0500

    Add postcard

--- github_web_pages/src/components/modules/Badges.js
+++ github_web_pages/src/components/modules/Badges.js
@@ -113,6 +113,29 @@ export default class Home extends Component {
                 />
             </Col>
         </Row>
+
+        <Row>
+          <ul className="content-block-list">
+            <li className="grid-50 tablet-grid-50">
+              <div className="content-block">
+                <div className="grid-70 tablet-grid-70 mobile-grid-70 content-meta">
+                  <strong>Achievement</strong>
+                  <h3>Thinking in Components</h3>
+                  <p>React Basics</p>
+                  <div className="content-actions-container">
+                    <span className="icon icon-complete"></span>
+                    <strong>Achieved</strong>
+                    <p>Sep 27, 2018</p>
+                  </div>
+                </div>
+                <div className="grid-30 tablet-grid-30 mobile-grid-30 achievement-hero">
+                  <img alt="Thinking in Components" src="https://achievement-images.teamtreehouse.com/badges_JavaScript_react_Stage2.png" />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </Row>
+        
       </Grid>
     );
   }
