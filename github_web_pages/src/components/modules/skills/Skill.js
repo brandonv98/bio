8cda4b13f57ba8ef9f6064e94d7ffca6880861e3
Brandon VanCamp <brandon@overtimewebdev.com>
2019-05-22 07:14:09 -0500

    Add TH api && loop threw badges arr.

--- github_web_pages/src/components/modules/skills/Skill.js
+++ github_web_pages/src/components/modules/skills/Skill.js
@@ -11,6 +11,7 @@ import {
 import PostCardCSS from './../../../PostCard.css';
 const PostCard = (props) => {
   console.log(props);
+  const corses = props.courses;
   return (
     <Col md={6}>
 
@@ -21,12 +22,12 @@ const PostCard = (props) => {
             <div className="content-block">
               <div className="grid-70 tablet-grid-70 mobile-grid-70 content-meta">
                 <strong>Achievement</strong>
-                <h3>Thinking in Components</h3>
-                <p>React Basics</p>
+                <h3>{props.name}</h3>
+                <p>{}</p>
                 <div className="content-actions-container">
                   <span className="icon icon-complete"></span>
                   <strong>Achieved</strong>
-                  <p>Sep 27, 2018</p>
+                  <p>{props.earned_date}</p>
                 </div>
               </div>
               <div className="grid-30 tablet-grid-30 mobile-grid-30 achievement-hero">
--- github_web_pages/src/components/modules/skills/Skills.js
+++ github_web_pages/src/components/modules/skills/Skills.js
@@ -6,21 +6,31 @@ import {NavLink, Link} from 'react-router-dom'; // ^^
 import Skill from './Skill';
 
 const Section = (props) => {
-
-  let stateData = [];
+  console.log(props.data.badges);
+  const sdData = [];
   // Loop thew badges data array.
   let dataBadges = props.data.badges;
   const badges = () => {
     for (const key in dataBadges) {
       const badge = dataBadges[key];
       const image = badge.icon_url;
-      let toPush = stateData.push(image);
+      const name = badge;
+      let toPush = sdData.push(badge);
     }
   };
   badges();
 
-  let skill = stateData.map((skill, i) => {
-    return <Skill image={skill}/>
+  // console.log(sdName);
+
+  let skill = sdData.map((skill, i) => {
+    // console.log(skill.courses[0]);
+    return <Skill 
+    key={skill.id}
+    image={skill.icon_url}
+    name={skill.name}
+    earned_date={skill.earned_date}
+    courses={skill.courses[0]}
+    />
   });
 
   return (
