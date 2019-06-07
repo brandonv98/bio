468f989d26505b013e12421fdf433965b9bebba6
Brandon VanCamp <brandon@overtimewebdev.com>
2019-06-07 10:50:09 -0500

    clean up

--- github_web_pages/src/components/modules/skills/Skills.js
+++ github_web_pages/src/components/modules/skills/Skills.js
@@ -6,8 +6,7 @@ import {NavLink, Link} from 'react-router-dom'; // ^^
 import Skill from './Skill';
 
 const Section = (props) => {
-  console.log(props.data.badges);
-  const sdData = [];
+  const sdData = []; // Store data
   // Loop thew badges data array.
   let dataBadges = props.data.badges;
   const badges = () => {
@@ -21,20 +20,19 @@ const Section = (props) => {
   badges();
 
   let skill = sdData.map((skill, i) => {
-    return <Skill 
-    key={skill.id}
-    image={skill.icon_url}
-    name={skill.name}
-    earned_date={skill.earned_date}
-    courses={skill.courses[0]}
-    />
+    return <Skill
+      key={skill.id}
+      image={skill.icon_url}
+      name={skill.name}
+      earned_date={skill.earned_date}
+      courses={skill.courses[0]}/>
   });
 
   return (
 
-      <ul className="content-block-list row">
-        {skill}
-      </ul>
+    <ul className="content-block-list row">
+      {skill}
+    </ul>
 
   );
 }
