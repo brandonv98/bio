7876c93f47a62f3c09acdfe76af26b6723f4e536
Brandon VanCamp <brandon@overtimewebdev.com>
2019-06-07 10:49:48 -0500

    Clean up

--- github_web_pages/src/components/modules/skills/Skill.js
+++ github_web_pages/src/components/modules/skills/Skill.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {Route, NavLink} from 'react-router-dom'; // Dependances are : RRD, Bootstrap
-import {
-  Nav,
-  Row,
-  Col,
-  Jumbotron,
-  Panel,
-  Grid
-} from 'react-bootstrap'; // ^^
 import PostCardCSS from './../../../PostCard.css';
 const PostCard = (props) => {
-  console.log(props);
   const corses = props.courses;
+  console.log(props);
   return (
         
           <li className="grid-50 tablet-grid-50 col-md-6">
@@ -22,7 +13,7 @@ const PostCard = (props) => {
                 <p>{}</p>
                 <div className="content-actions-container">
                   <span className="icon icon-complete"></span>
-                  {/* <strong>Achieved</strong> */}
+                  <strong>Achieved</strong>
                   {/* TODO: Add in date function */}
                   {/* <p>{props.earned_date}</p> */}
                 </div>
