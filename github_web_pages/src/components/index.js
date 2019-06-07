57bde6634849916163d861770727c63978ea07c0
Brandon VanCamp <brandon@overtimewebdev.com>
2019-06-07 10:02:35 -0500

    Clean up

--- github_web_pages/src/components/index.js
+++ github_web_pages/src/components/index.js
@@ -1,41 +1,37 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 // Components lib
+import {ProjectsData} from '../data/Projects'; // local DB
+
+// Load Modules
 import {
-  ProjectsData,
-} from '../data/Projects'; // local DB
-// import brandonvancamp2 from '../data/brandonvancamp2'; // local DB
-// import Head from './Header/Head'; // Header
-// import Container from './modules/Container'; // Home page
-// import GitHub from './GitHub'; // github API
-// import Footer from './Footer';
-import { Head, Container, GitHub, Footer, Badges, ContactForm} from './modules';
+  Head,
+  Container,
+  GitHub,
+  Footer,
+  Badges,
+  ContactForm
+} from './modules';
 // Dependencies
-import {
-  Route,
-  Switch
-} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 // Not Found Page
 import NotFound from './404Error/NotFound';
 
 export default class Home extends Component {
   render() {
     console.log('Looking for the source code? Just send me a email for it! Brandon@csdevs.io :)');
-    // console.log(brandonvancamp2);
-    // console.log(ProjectsData);
     return (
       <div className="App">
-          <Head />
-          <Switch> {/* SWITCH */}
-            <Route path='/:repo/github' component={GitHub} />
-            <Route exact path='/:badge/badges' component={Badges} />
-            <Route exact path='/:message/contact' component={ContactForm} />
-            <Route path='/bio' render={() =>
-                <Container data={ProjectsData}/> }/>
-            <Route component={NotFound} />
+        <Head/>
+        <Switch>
+          {/* SWITCH */}
+          <Route path='/:repo/github' component={GitHub}/>
+          <Route exact path='/:badge/badges' component={Badges}/>
+          <Route exact path='/:message/contact' component={ContactForm}/>
+          <Route path='/bio' render={() => <Container data={ProjectsData}/>}/>
+          <Route component={NotFound}/>
         </Switch>
-        <Route path='/' component={Footer} />
-      </div>);
+        <Route path='/' component={Footer}/>
+      </div>
+    );
   }
 }
