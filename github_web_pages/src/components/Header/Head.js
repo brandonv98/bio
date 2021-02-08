5d67b979ea69816a1eb42c214340e4af0d4f274c
Brandon VanCamp <brandon@vancamp.dev>
2021-02-07 23:22:36 -0600

    Update socal routes & cc

--- github_web_pages/src/components/Header/Head.js
+++ github_web_pages/src/components/Header/Head.js
@@ -10,12 +10,12 @@ const Head = (props) => {
            <Panel.Heading>
              <Panel.Title componentClass="h3">Hello, I'm Brandon</Panel.Title>
             </Panel.Heading>
-            <Panel.Body>Full Stack Engineer, problem solver. Team player. Tech enthusiast, and life long learner. Love to bring people together, and collaborate about new ideas.
+            <Panel.Body>Full-Stack Engineer, problem solver. Team player. Tech enthusiast, and life long learner. Love to bring people together, and collaborate about new ideas.
           </Panel.Body>
           <ul>
             <li><a href="https://github.com/brandonv98" target="_blank" rel="noreferrer noopener" className="social github">Github</a></li>
             <li><a href="https://www.linkedin.com/in/brandon-vancamp-123360150/" rel="noreferrer noopener" target="_blank" className="social linkedin">Linkedin</a></li>
-            <li><a href="https://twitter.com/brandondvancamp/" target="_blank" rel="noreferrer noopener" className="social twitter">Linkedin</a></li>
+            <li><a href="https://twitter.com/developerOneB/" target="_blank" rel="noreferrer noopener" className="social twitter">Linkedin</a></li>
           </ul>
         </Jumbotron>
           <Grid>
