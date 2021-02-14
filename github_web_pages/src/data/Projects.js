e026f12515c37078ac2232e9cddff18d66192c9b
Brandon VanCamp <brandon@vancamp.dev>
2021-02-14 06:52:02 -0600

    Update image location

--- github_web_pages/src/data/Projects.js
+++ github_web_pages/src/data/Projects.js
@@ -1,131 +1,183 @@
 export const ProjectsData = [{
 		title: "Flikr API Gallery",
-		img_src: require('../project_images/flikr_project/No-Results-Layout.png'),
-		img_src_next: require('../project_images/flikr_project/flikr_project.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/No-Results-Layout_vh9xj2.png',
+		img_src_next: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298658/flikr_project_dbyyym.png',
 		skills: ['JavaScript', 'ReactJS', 'JSX', 'API', 'CSS', 'HTML', 'Git', 'React-Router-Dom', 'GitHub'],
 		description: "Ooh ReactJS, now let me tell you about this project. First we get a connection with our API,  parse the information and store it in an array. For this I used axios, next we create separate components for organization so we can loop threw our data and append it onto the screen. React-Router-Dom was used to handle all of the routing, and keeping the DOM in sync.",
 		project_link_github: "React_Flickr_Gallery",
 		link_src: "React_Flickr_Gallery"
-  },
+	},
 	{
 		title: "Employee Directory Search",
-		img_src: require("../project_images/employee_directory/employee_directory.png"),
-		img_src_next: require("../project_images/employee_directory/employee_overlay.png"),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/employee_directory_ejcbgv.png',
+		img_src_next: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/employee_overlay_vdjyel.png',
 		skills: ['ReactJS', 'JavaScript', 'CSS', 'JSX', 'API', 'React-Router-Dom', 'Git', 'GitHub'],
 		description: "This project was originaly done static, with HTML, CSS & JS. However we decided to refactor it more dynamically with ReactJS.",
 		project_link_github: "employee_directory"
-  },
+	},
 	{
 		title: "Analytics Dashboard",
-		img_src: require('../project_images/analytics/dashboard.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298663/dashboard_nzlnfe.png',
 		skills: ['D3js', 'HTML', 'CSS', 'JavaScript', 'SVG', 'Git', 'GitHub'],
 		description: "D3.js was the clear choice to use for this dashboard to create all of the data and graphs. JavaScript to deliver user click events, user navigation, and handle popups/alerts. Example: When a user clicks on there notification, they will get a popup about there notifications. CSS was used for layout and styling.  SVG was the tool selected to create all icons.  Git was used for version control and to track changes in project files.  Finally, GitHub was selected for posting/sharing to the world",
 		project_link_github: "web-app-dashboard"
-  },
+	},
 	{
 		title: "Wheel Of Success",
-		img_src: require('../project_images/word_guessing/inGame.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298660/start_xtedb8.png',
+		img_src_next: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298660/inGame_eujk0q.png',
 		skills: ['Javascript', 'SVG', 'CSS', 'HTML', 'Git', 'GitHub'],
 		description: "Heavy JavaScript based project. JS was used to loop threw an array of phrases randomly. Once one is picked, that array is then appended to the screen, and you are ready to play! What happens next is to handle user input for each letter guessed. So we check threw each letter of the phrase of the picked array. If the user was correct then reveal that letter, if the user was incorrect we take a point off there score. Also we check to see if the game is currently still in play or if the user has lost. All this is done with JavaScript.",
 		project_link_github: "Game_Show_App"
 	},
 	{
 		title: "Tic Tac Toe",
-		img_src: require('../project_images/tictactoe-inprogress.png'),
-		img_src_next: require("../project_images/tictactoe-start.png"),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298662/tictactoe-start_utcslk.png',
+		img_src_next: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/tictactoe-inprogress_cpehvg.png',
 		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization'],
 		description: "Coming soon...",
 		project_link_github: "tic_tac_toe",
 		website_domain_name: "brandonv98.github.io/tic_tac_toe"
-  },
+	},
 	{
 		title: "Circle Of Death",
-		img_src: require('../project_images/circle-of-death-start.png'),
-		img_src_next: require("../project_images/circle-of-death-ingame.png"),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298661/circle-of-death-start_ysek0d.png',
+		img_src_next: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298659/circle-of-death-ingame_rnktou.png',
 		skills: ['HTML', 'CSS', 'Javascript', 'Git', 'GitHub'],
 		description: "Circle of death drinking game, revamped for drinking fun with friends. Inspired by Jake Airgood.",
 		project_link_github: "circle_of_death_game"
-  },
-	{
-		title: "Blue Crush QC",
-		img_src: require('../project_images/bluecrushqc_full_Desktop.png'),
-		skills: ['HTML', 'CSS', 'Javascript', 'PHP', 'WordPress', 'Search Engine Optimization'],
-		description: "Project was for a local bussiness, specializing in tanning, massage therapy, and beauty. Used JavaScript to handle the slideshow. CSS to make everything nice and cute. HTML for the initial structure. WordPress, and PHP for the news feed, as well for the admin back-end.",
-		project_link_github: "",
-		// project_link: ""
-  },
+	},
 	{
 		title: "Random Quote Generator",
-		img_src: require('../project_images/brandonv98.github.io_Random_Quote_Generator_.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298662/brandonv98.github.io_Random_Quote_Generator__klvuex.png',
 		skills: ['JavaScript', 'HTML', 'CSS', 'Git', 'GitHub'],
 		description: "",
 		project_link_github: "Random_Quote_Generator"
-  },
+	},
 	{
 		title: "Library Manager",
-		img_src: require('../project_images/localhost_3000_novels.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/localhost_3000_novels_dfwi3d.png',
 		skills: ['JavaScript', 'HTML', 'CSS', 'Git', 'GitHub', 'SQL', 'Express', 'NodeJS'],
 		description: "",
 		project_link_github: "library-manager"
-  },
+	},
 	{
 		title: "Sketch Template",
-		img_src: require('../project_images/j3.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298658/j3_psse6f.png',
 		skills: ['Sketch', 'HTML', 'SVG', 'CSS', 'JavaScript', 'jQuery', 'BootStrap'],
 		description: "Developed with Sketch. Then brought to life with HTML, CSS, and JavaScript. Most of the CSS used was from BootStrap. SVG's used for footer icons. JavaScript & jQuery handled all the functionality with the slideshow and image overlay's.",
 		project_link_github: ""
-  },
+	},
 	{
-		title: "goode words",
-		img_src: require('../project_images/goodewords.png'),
-		skills: ['WordPress', 'Strip', 'HTML', 'SASS', 'JavaScript', 'PHP', 'Search Engine Optimization'],
-		description: "",
-		project_link_github: ""
-  },
+		title: "Blue Crush QC",
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298663/bluecrushqc_full_Desktop_y9dbjg.png',
+		skills: ['HTML', 'CSS', 'Javascript', 'PHP', 'WordPress', 'Search Engine Optimization'],
+		description: "Project was for a local bussiness, specializing in tanning, massage therapy, and beauty. Used JavaScript to handle the slideshow. CSS to make everything nice and cute. HTML for the initial structure. WordPress, and PHP for the news feed, as well for the admin back-end.",
+		project_link_github: "",
+		project_link: "blue_crush_project_link"
+	},
+	// {
+	// 	title: "Goode Words",
+	// 	img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298657/goodewords_dsowgp.png',
+	// 	skills: ['WordPress', 'Strip', 'HTML', 'SASS', 'JavaScript', 'PHP', 'Search Engine Optimization'],
+	// 	description: "",
+	// 	project_link_github: ""
+	// 	// website_domain_name: "jordan-holmes-sales-website.herokuapp.com"
+	// },
 	{
 		title: "Tuff Girl Fit Wear",
-		img_src: require('../project_images/tuff_girl.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298659/tuff_girl_hxjfws.png',
 		skills: ['WIX', 'API', 'Communication', 'DNS Linking', 'Explicit Documentation', 'JSON'],
-		description: "",
-		project_link_github: ""
-  },
-	{
-		title: "Monson Towing",
-		img_src: require('../project_images/towing.png'),
-		skills: ['ReactJS', 'API', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization'],
-		description: "",
-		project_link_github: ""
-  },
-	{
-		title: "Fat Bat Pipes",
-		img_src: require('../project_images/fatbatpipes.png'),
-		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization'],
 		description: "",
 		project_link_github: ""
 	},
 	{
+		title: "Monson Towing",
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298663/towing_jlk4sb.png',
+		skills: ['ReactJS', 'API', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization'],
+		description: "",
+		project_link_github: ""
+	},
+	// {
+	// 	title: "Monson Graphics",
+	// 	img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298661/MonsonSnG_hjccsu.png',
+	// 	skills: ['WordPress', 'API', 'PHP', 'CSS', 'JavaScript', 'Search Engine Optimization', 'Design', 'Image Mapping', 'e-commerce'],
+	// 	description: "",
+	// 	project_link_github: ""
+	// },
+	{
+		title: "OverTime Web Dev",
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298660/overtimewebdev.com_w1mv1r.png',
+		skills: ['WordPress', 'API', 'PHP', 'CSS', 'JavaScript', 'Search Engine Optimization', 'DevOps', 'Configurations', 'Heavy Site Cache & Purge'],
+		description: "",
+		project_link_github: "",
+		website_domain_name: "overtimewebdev.com"
+	},
+	// {
+	// 	title: "Fat Bat Pipes",
+	// 	img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298660/fatbatpipes_gyfsp1.png',
+	// 	skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization'],
+	// 	description: "",
+	// 	project_link_github: ""
+	// },
+	{
 		title: "Jordan Holmes Sales",
-		img_src: require('../project_images/jordan-holmes-screen-shot.png'),
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298661/jordan-holmes-screen-shot_c2umls.png',
+		// img_src_next: '',
 		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization', 'NodeJS', 'Express', 'NPM'],
-		description: "Coming soon...",
+		description: "",
 		project_link_github: "",
 		website_domain_name: "jordan-holmes-sales-website.herokuapp.com"
 	},
 	{
 		title: "Palmer Hills Place",
-		img_src: require('../project_images/palmer-hills-place-screen-shot.png'),
-		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization'],
+		img_src: 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613305361/project%20images/palmer-hills-place-screen-strech_djv4cv.png',
+		// img_src_next: '',
+		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization', 'Heroku', 'SCRUM', 'API', 'GIT'],
 		description: "",
 		project_link_github: "",
 		website_domain_name: "palmerhillsplace.com"
-	}
+	},
+	{
+		title: "Coming 2021",
+		img_src: '',
+		// img_src_next: '',
+		skills: ['WordPress', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization', 'Heroku', 'SCRUM', 'API', 'GIT'],
+		description: "Stay tuned for new project rolling out in 2021...",
+		project_link_github: "",
+		website_domain_name: ""
+	},
+	// {
+	// 	title: "Coming 2021",
+	// 	img_src: '',
+	// 	// img_src_next: '',
+	// 	skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization', 'AWS', 'SCRUM', 'API', 'GIT', 'Design', 'DevOps'],
+	// 	description: "Stay tuned for new project rolling out in 2021...",
+	// 	project_link_github: "",
+	// 	website_domain_name: ""
+	// },
 ];
+
+
 
 
 // {
 // 	title: ,
-// 	img_src: require('../'),
+// 	img_src: '../',
 // 	skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'Search Engine Optimization'],
+//		skills: ['ReactJS', 'JSX', 'CSS', 'JavaScript', 'NodeJS', 'Express', 'NPM', 'Search Engine Optimization', 'Heroku', 'SCRUM', 'API', 'GIT'],
 // 	description: "",
-// 	project_link_github: ""
+// 	project_link_github: "",
+//		website_domain_name: ""
 // }
+
+
+
+// ELM Application - MacOS App Store Images
+// images: [
+// 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298659/About_skaohg.png'.
+// 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298659/Multi_Shot_mxa5sl.png,'
+// 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298659/ELMHome_j4iy34.png',
+//  'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298658/Services_nkmkxk.png',
+// 'https://res.cloudinary.com/https-vancamp-dev/image/upload/v1613298658/Hours_rrqutm.png'.
+// ]
