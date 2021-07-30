8019329795ed223f12074db2b326cdce122f778b
Brandon VanCamp <brandon@vancamp.dev>
2021-07-30 07:56:27 -0500

    Create & Add GitHub OAuth integration

---
+++ bio-ui/src/components/GitHubLogin.js
@@ -0,0 +1,12 @@
+var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://github.com/login/oauth/token?user=email&read=user&read=org',
+  'headers': {
+    'Cookie': 'Cookie_1=value; _device_id=dfb8231e3bed59033ac2fb924d32a3fe; _octo=GH1.1.518464387.1623399591; logged_in=no'
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
