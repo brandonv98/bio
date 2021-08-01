1f1ae04b26bb90d4d61fa048da8c22faf74a165b
Brandon VanCamp <brandon@vancamp.dev>
2021-08-01 10:43:58 -0500

    Add GitHub component

--- bio-ui/src/components/modules/index.js
+++ bio-ui/src/components/modules/index.js
@@ -1,10 +1,11 @@
 // Import pages components
 import Head from '../Header/Head'; // Header
 import Container from './Container'; // Home page
-import GitHub from '../GitHub'; // github API
+import GitHub from '../GitHub'; // GitHub API
+import GitHubLogin from '../GitHubLogin'; // GitHub API
 import Footer from '../Footer';
 import Badges from './Badges'; // Badges Json DB
 import ContactForm from './ContactForm';
 
 // Export
-export { Head, Container, GitHub, Footer, Badges, ContactForm};
+export { Head, Container, GitHub, Footer, Badges, ContactForm, GitHubLogin};
